58a5d842e3cf858db4537df4c4416ba0ed08b29e
Jason Bittel <jason.bittel@gmail.com>
2015-02-03 09:46:40 -0800

    Move admin URL include outside of app

--- shopify/order_payment/urls.py
+++ shopify/order_payment/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import include
 from django.conf.urls import patterns
 from django.conf.urls import url
-from django.contrib import admin
 
 from .views import OrderPaymentView
 
 
 urlpatterns = patterns('',
     url(r'^order-payment/$', OrderPaymentView.as_view(), name='order_payment'),
-    url(r'^admin/', include(admin.site.urls)),
 )
--- shopify/urls.py
+++ shopify/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import include
 from django.conf.urls import patterns
 from django.conf.urls import url
+from django.contrib import admin
 
 
 urlpatterns = patterns('',
     url(r'', include('order_payment.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
