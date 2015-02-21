ab19642cfcfea0a437727a81e39d0802d6c0dedf
Jason Bittel <jason.bittel@gmail.com>
2015-02-20 16:02:44 -0800

    Fix manager email recipients list

--- shopify/product/tasks.py
+++ shopify/product/tasks.py
@@ -55,7 +55,7 @@ def email_journal_vouchers_import():
 def mail_managers(subject, message, attachment=None, fail_silently=False):
     """Send email to managers, with an optional attachment."""
     mail = EmailMessage("%s%s" % (settings.EMAIL_SUBJECT_PREFIX, subject),
-            message, to=[m[1] for m in settings.MANAGERS])
+            message, to=settings.MANAGERS)
     if attachment:
         mail.attach(attachment.getname(), attachment.getvalue(), 'text/csv')
     mail.send(fail_silently=fail_silently)
