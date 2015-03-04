0b142fb4e977bd85c913d9377fcb57c9a4a0e6b0
Jason Bittel <jason.bittel@gmail.com>
2015-03-04 14:22:57 -0800

    Edit product notifications inline with products

--- shopify/notification/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import ProductNotification
-
-
-admin.site.register(ProductNotification)
--- shopify/product/admin.py
+++ shopify/product/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
 from .models import Product
+from notification.models import ProductNotification
+
+
+class ProductNotificationInline(admin.TabularInline):
+    max_num = 1
+    model = ProductNotification
 
 
 class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductNotificationInline]
     list_display = ('description', 'product_type', 'account_number')
     ordering = ('description',)
     readonly_fields = ('description', 'product_type', 'product_id')
