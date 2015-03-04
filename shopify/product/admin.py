3849a5a842137a29ce06b5b7e027c8f8efd4e00e
Jason Bittel <jason.bittel@gmail.com>
2015-03-04 14:18:23 -0800

    Make Shopify product fields readonly

--- shopify/product/admin.py
+++ shopify/product/admin.py
@@ -6,6 +6,7 @@ from .models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('description', 'product_type', 'account_number')
     ordering = ('description',)
+    readonly_fields = ('description', 'product_type', 'product_id')
 
     class Meta:
         model = Product
