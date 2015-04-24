47c20cf9ca70f417b3eaf14bc733ebd4a958ad9d
Jason Bittel <jason.bittel@gmail.com>
2015-04-24 10:01:05 -0700

    Add test for orders paid webhook view

--- shopify/webhooks/tests/test_views.py
+++ shopify/webhooks/tests/test_views.py
@@ -9,6 +9,27 @@ from webhooks.models import Webhook
 
 
 @pytest.mark.django_db
+class TestOrders:
+    filename = 'fixtures/orders-paid.json'
+
+    def test_orders_paid(self, json, hmac):
+        Product.objects.create(product_id=123456, description='Sledgehammer')
+        Product.objects.create(product_id=12345, description='Wire Cutter')
+        with patch('requests.post') as mock:
+            mock.return_value.status_code = 200
+            mock.return_value.raise_for_status.return_value = None
+            mock.return_value.raise_for_status()
+            mock.return_value.json.return_value = {'webhook': {'id': 12345}}
+            mock.return_value.json()
+            hook = Webhook.objects.create(topic='orders/paid')
+
+        c = Client()
+        c.post(hook.path, data=json, content_type='text/json',
+               HTTP_X_SHOPIFY_HMAC_SHA256=hmac)
+        assert Transaction.objects.count() == 2
+
+
+@pytest.mark.django_db
 class TestRefunds:
     filename = 'fixtures/refunds-create.json'
 
